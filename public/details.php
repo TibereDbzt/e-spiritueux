2dc8ff21c02c8fd246cbd5af52a660131c60a1ac
Zhoulin Yu <zhoulin.yu@me.com>
2021-05-07 13:25:11 +0200

    try to add carousel to show multi-image
    carousel doesn't work

--- public/details.php
+++ public/details.php
@@ -6,6 +6,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <!-- load bootstrap styles -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-wEmeIV1mKuiNpC+IOBjI7aAzPcEZeedi5yW5f2yOq55WWLwNGmvvx4Um1vskeMj0" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/js/bootstrap.min.js" integrity="sha384-lpyLfhYuitXl2zRZ5Bn2fqnhNAKOAaM/0Kr9laMspuaMiZfGmfwRNFh8HlMy49eQ" crossorigin="anonymous"></script>
     <!-- load a reset file to standardize browsers -->
     <link rel="stylesheet" href="./styles/reset.css">
     <!-- generic styles -->
@@ -36,47 +38,83 @@
     getDebug();
 
     require MODEL_PATH . 'Product.model.php';
+    require_once MODEL_PATH . 'Category.model.php';
 
     $id = $_GET['id'];
     $product = ProductModel::getProductById($id);
     $nom = $product->getName();
-    $categorie = $product->getCategoryId();
+    $categorieID = $product->getCategoryId();
     $origine = $product->getOrigin();
     $prix = $product->getPrice();
     $volume = $product->getVolume();
     $alcoholLevel = $product->getAlcoholLevel();
     $description = $product->getDescription();
     $imageName= $product->getImageName();
+    $categorie = CategoryModel::getCategoryById($categorieID);
+    $categorieName= $categorie->getCategoryName();
+
 
 
 
     ?>
-
+    <div class="padding_layout_1 ">
     <div class="row">
         <div class="col-xl-6 col-lg-12 col-md-12">
             <div class="product_detail_feature_img hizoom hi2">
                 <div class='hizoom hi2'> <img src="../data/images/<?php echo $imageName ?>.png" alt="#" /> </div>
             </div>
+            <!-- Image Slider -->
+            
+
+            <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+                <div class="carousel-inner">
+                    <div class="carousel-item active">
+                        <img class="d-block w-100" src="../data/images/bg3.jpeg" alt="First slide">
+                    </div>
+                    <div class="carousel-item">
+                        <img class="d-block w-100" src="../data/images/bg1.jpg" alt="Second slide">
+                    </div>
+                    <div class="carousel-item">
+                        <img class="d-block w-100" src="../data/images/bg2.jpg" alt="Third slide">
+                    </div>
+                </div>
+                <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                    <span class="sr-only">Previous</span>
+                </a>
+                <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                    <span class="sr-only">Next</span>
+                </a>
+            </div>
+            <!-- end of Image slider -->
         </div>
         <div class="col-xl-6 col-lg-12 col-md-12 product_detail_side detail_style1">
             <div class="product-heading">
-                <h1><?php echo $nom ?></h1>
+                <h2><?php echo $nom ?></h2>
+                </div>
+            <div class="product-info">
+                <h5><?php echo $categorieName ?></h5>
+                <h5><?php echo $origine ?></h5>
+                <h5><?php echo $volume ?> cl</h5>
+                <h5>Vol <?php echo $alcoholLevel ?> %</h5>
             </div>
-            <div class="product-detail-side"> <span class="new-price"><?php echo $prix ?></span></div>
+            <div class="product-detail-side"> <span class="price">Prix - <?php echo $prix ?> €</span></div>
             <div class="detail-contant">
                 <p><?php echo $description ?><br>
                 </p>
-                <form class="cart" method="post" action="it_cart.html">
-                    <div class="quantity">
-                        <input step="1" min="1" max="5" name="quantity" value="1" title="Qty" class="input-text qty text" size="4" type="number">
-                    </div>
-                    <button type="submit" class="btn sqaure_bt">Add to cart</button>
-                </form>
             </div>
+            <form class="cart" method="post" action="cart.html">
+                <div class="quantity">
+                    <input step="1" min="1" max="5" name="quantity" value="1" title="Qty" class="input-text qty text" size="4" type="number">
+                </div>
+                <button type="submit" class="btn">Add to cart</button>
+            </form>
         </div>
+    </div>
     </div>
 
     <!-- load bootstrap scripts -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-p34f1UUtsS3wqzfto5wAAmdvj+osOnFyQFpp4Ua3gs/ZVWx6oOypYoCJhGGScy+8" crossorigin="anonymous"></script>
+
 </body>
 </html>
